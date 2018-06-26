c4ae44f2c6038c21cb17124a34401d0c5526161f
Sebastian Marcet <smarcet@gmail.com>
2018-06-26 00:45:36 -0300

    [smarcet] * addd sitemap.xml ingestion to crawler

--- conf/nutch/crawler.sh
+++ conf/nutch/crawler.sh
@@ -6,13 +6,19 @@ CORE=$1
 CRAWL_ID=$2
 SEEDS=$3
 ITERATIONS=$4
+DOMAIN=$5
+TOP=$6
+
+echo "trying to get site map for $DOMAIN..."
+
+curl $DOMAIN/sitemap.xml | grep -e loc | sed 's|<loc>\(.*\)<\/loc>$|\1|g' > $SEEDS/sitemap.txt
 
 echo "crawling for core $CORE crawl_id $CRAWL_ID seed $SEEDS";
 
 for i in $(seq ${ITERATIONS}); do
       echo "Iteration $i"
       $NUTCH_HOME/runtime/local/bin/nutch inject $SEEDS -crawlId $CRAWL_ID
-      $NUTCH_HOME/runtime/local/bin/nutch generate -topN 50 -crawlId $CRAWL_ID
+      $NUTCH_HOME/runtime/local/bin/nutch generate -topN $TOP -crawlId $CRAWL_ID
       $NUTCH_HOME/runtime/local/bin/nutch fetch -all -crawlId $CRAWL_ID
       $NUTCH_HOME/runtime/local/bin/nutch parse -all -crawlId $CRAWL_ID
       $NUTCH_HOME/runtime/local/bin/nutch updatedb -all -crawlId $CRAWL_ID
