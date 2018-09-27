12f2ac944b7ab208416fb1f96b3a61c38a4332f4
Sebastian Marcet <smarcet@gmail.com>
2018-09-27 15:40:53 -0300

    [smarcet] * tweaks on nutch params

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,9 +10,9 @@ service cron start
 
 # run crawler first time
 
-. /root/env.sh && $NUTCH_HOME/crawler.sh www-openstack 1 $NUTCH_HOME/urls/www $DEFAULT_DEPTH https://www.openstack.org $DEFAULT_TOP $NUTCH_LOCAL_CONF/www
-. /root/env.sh && $NUTCH_HOME/crawler.sh docs-openstack 2 $NUTCH_HOME/urls/docs $DEFAULT_DEPTH https://docs.openstack $DEFAULT_TOP  $NUTCH_LOCAL_CONF/docs
-. /root/env.sh && $NUTCH_HOME/crawler.sh superuser-openstack 3 $NUTCH_HOME/urls/superuser $DEFAULT_DEPTH "" $DEFAULT_TOP $NUTCH_LOCAL_CONF/superuser
-. /root/env.sh && $NUTCH_HOME/crawler.sh blog 4 $NUTCH_HOME/urls/blog $DEFAULT_DEPTH "" $DEFAULT_TOP $NUTCH_LOCAL_CONF/blog
+. /root/env.sh && $NUTCH_HOME/crawler.sh www-openstack 1 $NUTCH_HOME/urls/www $DEFAULT_DEPTH https://www.openstack.org $DEFAULT_TOP $NUTCH_LOCAL_CONF/www >> /var/log/nutch_cron_www.log
+. /root/env.sh && $NUTCH_HOME/crawler.sh docs-openstack 2 $NUTCH_HOME/urls/docs 250 https://docs.openstack 10000  $NUTCH_LOCAL_CONF/docs >> /var/log/nutch_cron_docs.log
+. /root/env.sh && $NUTCH_HOME/crawler.sh superuser-openstack 3 $NUTCH_HOME/urls/superuser $DEFAULT_DEPTH "" $DEFAULT_TOP $NUTCH_LOCAL_CONF/superuser >> /var/log/nutch_cron_superuser.log
+. /root/env.sh && $NUTCH_HOME/crawler.sh blog 4 $NUTCH_HOME/urls/blog 50 "" 10000 $NUTCH_LOCAL_CONF/blog >> /var/log/nutch_cron_blog.log
 
 exec "$@";
